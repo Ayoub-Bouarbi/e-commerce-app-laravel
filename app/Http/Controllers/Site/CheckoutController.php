5563aa32df81668fb9cf5ee725127f76e51e6336
Ayoub Bouarbi <Bouarbi.ayoub1997@gmail.com>
2020-07-11 22:53:05 +0200

    add payment processing

--- app/Http/Controllers/Site/CheckoutController.php
+++ app/Http/Controllers/Site/CheckoutController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers\Site;
 
 use App\Contracts\OrderContract;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Services\PaypalService;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 class CheckoutController extends Controller
 {
     protected $orderContract;
+    protected $paypal;
 
-    public function __construct(OrderContract $orderContract)
+    public function __construct(OrderContract $orderContract,PaypalService $paypal)
     {
         $this->orderContract = $orderContract;
+        $this->paypal = $paypal;
     }
 
     public function getCheckout()
@@ -24,8 +29,30 @@ class CheckoutController extends Controller
     {
         // Before storing the order we should implement the
         // request validation which I leave it to you
-        $order = $this->orderContract->storeOrderDetails($request->all());
+        $order = $this->orderRepository->storeOrderDetails($request->all());
 
-        dd($order);
+        // You can add more control here to handle if the order
+        // is not stored properly
+        if ($order) {
+            $this->payPal->processPayment($order);
+        }
+
+        return redirect()->back()->with('message','Order not placed');
+    }
+    public function complete(Request $request)
+    {
+        $paymentId = $request->input('paymentId');
+        $payerId = $request->input('PayerID');
+
+        $status = $this->payPal->completePayment($paymentId, $payerId);
+
+        $order = Order::where('order_number', $status['invoiceId'])->first();
+        $order->status = 'processing';
+        $order->payment_status = 1;
+        $order->payment_method = 'PayPal -'.$status['salesId'];
+        $order->save();
+
+        Cart::clear();
+        return view('site.pages.success', compact('order'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     {
         return $this->first_name. ' '. $this->last_name;
     }
+    
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
